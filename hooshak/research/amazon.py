85c13ef65181e657cbc69d847e568be812a7cce4
perfect <m.aali.pro@gmail.com>
2017-07-26 01:03:27 +0430

    compliting research

--- hooshak/research/amazon.py
+++ hooshak/research/amazon.py
@@ -13,6 +13,18 @@ ratings_Movies_and_TV.csv -> ~ 4,600,000 record
 ratings_Movies_and_TV.csv -> ~ Unique user number:  2088620
 ratings_Movies_and_TV.csv -> ~ Unique entity number: 200941
 
+
+beautiful_ratings_Movies_and_TV.csv:
+
+    Rows number:          1,932,230
+    Unique user number:     139,138
+    Unique entity number:   153,436
+
+
+    first: ['A29X9U79LWG6YT', 'B00JH4SZWK', 4, 1406073600]  --->  Wednesday, July 23, 2014 12:00:00 AM
+    last:  ['A1127LKNR08JJK', '630174411X', 4, 879379200]   --->  Thursday, November 13, 1997 12:00:00 AM
+
+
 """
 
 
@@ -24,7 +36,8 @@ class User(HooshakUserMixin):
         return self._uid
 
 
-csv_uri = '../../datasource/ratings_Movies_and_TV.csv'
+# csv_uri = '../../datasource/ratings_Movies_and_TV.csv'
+csv_uri = '../../datasource/beautiful_ratings_Movies_and_TV.csv'
 csv_delimiter = ','
 
 
@@ -126,37 +139,79 @@ def calculate_user_activities():
         print(f'Sum: {_sum}')
 
         selected_index = 1900000
-        print(f'Selected index: {selected_index} is: {sorted(user_dict.items(), key=operator.itemgetter(1))[selected_index][1]}')
+        print(
+            f'Selected index: {selected_index} is: {sorted(user_dict.items(), key=operator.itemgetter(1))[selected_index][1]}')
+
+
+def check_duplicate_doesnt_exists():
+    # TODO: Implement
+    pass
 
 
 def make_beautiful():
+    remained_user_list = []
+    beautiful_rows = []
+    user_dict = {}
+
     with open(csv_uri, 'r') as csv_file:
-        user_dict = {}
         csv_reader = csv.reader(csv_file, delimiter=csv_delimiter)
         for row in csv_reader:
+            row[2] = int(row[2][0])
+            row[3] = int(row[3])
             if row[0] in user_dict:
-                user_dict.update({row[0]: user_dict[row[0]] + 1})
+                user_dict[row[0]].append(row)
             else:
-                user_dict[row[0]] = 1
+                user_dict[row[0]] = [row]
 
-        remained_user_list = []
-        should_delete_number = 1900000
-        for key, value in sorted(user_dict.items(), key=operator.itemgetter(1)):
-            if should_delete_number < 0:
-                remained_user_list.append(key)
-            should_delete_number -= 1
+    print(f'Number of all users: {len(user_dict)}')
 
-        print(f'Number of remained users: {len(remained_user_list)}')
+    # with open(csv_uri, 'r') as csv_file:
+    #     csv_reader = csv.reader(csv_file, delimiter=csv_delimiter)
+    #     counter = 0
+    #     for row in csv_reader:
+    #         if row[0] in remained_user_list:
+    #             beautiful_rows.append(row)
+    #             counter += 1
+    #             print(counter)
+    #
+    # beautiful_rows.sort(key=operator.itemgetter(3), reverse=True)
+    beautiful_csv_uri = '../../datasource/beautiful_ratings_Movies_and_TV.csv'
+    #
+    # print('Writing...')
+    #
+    # with open(beautiful_csv_uri, 'w', newline='') as csvfile:
+    #     spamwriter = csv.writer(csvfile, delimiter=',',
+    #                             quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    #     for row in beautiful_rows:
+    #         spamwriter.writerow(row)
+
+    beautiful_counter = 0
+    for key, value in user_dict.items():
+        if len(value) >= 5:
+            for row in value:
+                beautiful_rows.append(row)
+            beautiful_counter += 1
+    print(f'Beautiful count: {beautiful_counter}')
+
+    beautiful_rows.sort(key=operator.itemgetter(3), reverse=True)
+    print(beautiful_rows[0])
+    print(beautiful_rows[-1])
+
+    with open(beautiful_csv_uri, 'w', newline='') as csvfile:
+        spamwriter = csv.writer(csvfile, delimiter=',',
+                                quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        for row in beautiful_rows:
+            spamwriter.writerow(row)
+
 
 if __name__ == '__main__':
     warehouse = hooshex.warehouse
 
-    # calculate_rows()
-    # calculate_unique_users()
-    # calculate_unique_entities()
-    # calculate_user_activities()
-
-
+    calculate_rows()
+    calculate_unique_users()
+    calculate_unique_entities()
+    calculate_user_activities()
+    # make_beautiful()
 
     timer = HTimer()
 
