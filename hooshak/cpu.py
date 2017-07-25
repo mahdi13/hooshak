ca3defa1ed667400fee73b8747b09b3f7d0c119f
perfect <m.aali.pro@gmail.com>
2017-07-26 00:14:12 +0430

    compliting research

--- hooshak/cpu.py
+++ hooshak/cpu.py
@@ -7,22 +7,54 @@ class CPU:
     def __init__(self, g: Graph):
         self.g = g
 
-    def calculate_smart_score(self, user: HooshakUserMixin, entity: HooshakEntityMixin):
+    # def calculate_smart_score(self, user: HooshakUserMixin, entity: HooshakEntityMixin):
+    #     out = []
+    #     from hooshak.context import hooshex
+    #     for path in all_paths(
+    #             g=self.g,
+    #             source=hooshex.warehouse.get_user_v_by_uid(user.get_hooshak_uid()),
+    #             target=hooshex.warehouse.get_entity_v_by_uid(entity.get_hooshak_uid()),
+    #             # weights=hooshex.warehouse.eprop_value,
+    #             cutoff=3
+    #
+    #     ):
+    #         if len(path) == 4:
+    #             out.append((
+    #                            hooshex.warehouse.eprop_value[self.g.edge(path[0], path[1])] *
+    #                            hooshex.warehouse.eprop_value[self.g.edge(path[1], path[2])] *
+    #                            hooshex.warehouse.eprop_value[self.g.edge(path[2], path[3])]) ** (1. / 3.)
+    #                        )
+    #             # elif len(path) == 2:
+    #             #     return None
+    #             #
+    #             #     e = self.g.edge(path[0], path[1])
+    #             #     self.g.remove_edge(e)
+    #             #
+    #             #     out = self.calculate_smart_score(user=user, entity=entity)
+    #             #
+    #             #     self.g.add_edge(path[0], path[1])
+    #             #
+    #             #     # return out
+    #             #     # out.append('shit')
+    #
+    #         return out
+
+    def calculate_smart_score(self, user_uid, entity_uid):
         out = []
         from hooshak.context import hooshex
         for path in all_paths(
                 g=self.g,
-                source=hooshex.warehouse.get_user_v_by_uid(user.get_hooshak_uid()),
-                target=hooshex.warehouse.get_entity_v_by_uid(entity.get_hooshak_uid()),
+                source=hooshex.warehouse.get_user_v_by_uid(user_uid),
+                target=hooshex.warehouse.get_entity_v_by_uid(entity_uid),
                 # weights=hooshex.warehouse.eprop_value,
                 cutoff=3
 
         ):
             if len(path) == 4:
                 out.append((
-                               hooshex.warehouse.eprop_value[self.g.edge(path[0], path[1])] *
-                               hooshex.warehouse.eprop_value[self.g.edge(path[1], path[2])] *
-                               hooshex.warehouse.eprop_value[self.g.edge(path[2], path[3])]) ** (1. / 3.)
+                               hooshex.warehouse.g.properties[('e', 'value')][self.g.edge(path[0], path[1])] *
+                               hooshex.warehouse.g.properties[('e', 'value')][self.g.edge(path[1], path[2])] *
+                               hooshex.warehouse.g.properties[('e', 'value')][self.g.edge(path[2], path[3])]) ** (1. / 3.)
                            )
                 # elif len(path) == 2:
                 #     return None
--- hooshak/research/amazon.py
+++ hooshak/research/amazon.py
@@ -1,8 +1,19 @@
 import csv
+import functools
+import operator
 
+from hooshak import wise
 from hooshak.context import hooshex
 from hooshak.modeling import HooshakUserMixin
 from hooshak.tools import HTimer
+
+"""
+
+ratings_Movies_and_TV.csv -> ~ 4,600,000 record
+ratings_Movies_and_TV.csv -> ~ Unique user number:  2088620
+ratings_Movies_and_TV.csv -> ~ Unique entity number: 200941
+
+"""
 
 
 class User(HooshakUserMixin):
@@ -13,10 +24,11 @@ class User(HooshakUserMixin):
         return self._uid
 
 
-def read():
-    csv_uri = '../../datasource/ratings_Movies_and_TV.csv'
-    csv_delimiter = ','
+csv_uri = '../../datasource/ratings_Movies_and_TV.csv'
+csv_delimiter = ','
 
+
+def read():
     warehouse = hooshex.warehouse
 
     with open(csv_uri, 'r') as csv_file:
@@ -42,20 +54,116 @@ def read():
             warehouse.add_activity(user_uid, entity_uid, int(value[0]), int(timestamp))
 
             row_number += 1
-            if row_number == 100000:
+            if row_number == 1000000:
                 break
 
         print(f'Total rows: {row_number}')
 
+
+def find_usable():
+    with open(csv_uri, 'r') as csv_file:
+        row_number = 0
+        usable_number = 0
+        csv_reader = csv.reader(csv_file, delimiter=csv_delimiter)
+        for row in csv_reader:
+            if 1000000 < row_number:
+                try:
+                    hooshex.warehouse.get_user_v_by_uid(row[0])
+                    hooshex.warehouse.get_entity_v_by_uid(row[1])
+                    print(f'Use this: {row[0]},{row[1]} it should be: {row[2]}')
+                    usable_number += 1
+                except KeyError:
+                    pass
+                if usable_number > 10:
+                    return
+            row_number += 1
+
+
+def calculate_rows():
+    with open(csv_uri, 'r') as csv_file:
+        row_number = 0
+        csv_reader = csv.reader(csv_file, delimiter=csv_delimiter)
+        for row in csv_reader:
+            row_number += 1
+        print(f'Rows number: {row_number}')
+
+
+def calculate_unique_users():
+    with open(csv_uri, 'r') as csv_file:
+        user_list = []
+        csv_reader = csv.reader(csv_file, delimiter=csv_delimiter)
+        for row in csv_reader:
+            user_list.append(row[0])
+
+        print(f'Unique user number: {len(list(set(user_list)))}')
+
+
+def calculate_unique_entities():
+    with open(csv_uri, 'r') as csv_file:
+        entity_list = []
+        csv_reader = csv.reader(csv_file, delimiter=csv_delimiter)
+        for row in csv_reader:
+            entity_list.append(row[1])
+
+        print(f'Unique entity number: {len(list(set(entity_list)))}')
+
+
+def calculate_user_activities():
+    with open(csv_uri, 'r') as csv_file:
+        user_dict = {}
+        csv_reader = csv.reader(csv_file, delimiter=csv_delimiter)
+        for row in csv_reader:
+            if row[0] in user_dict:
+                user_dict.update({row[0]: user_dict[row[0]] + 1})
+            else:
+                user_dict[row[0]] = 1
+
+        _sum = 0
+        for key, value in sorted(user_dict.items(), key=operator.itemgetter(1)):
+            # print(f'User: {key} ---- Activity number: {value}')
+            _sum += value
+
+        print(f'Sum: {_sum}')
+
+        selected_index = 1900000
+        print(f'Selected index: {selected_index} is: {sorted(user_dict.items(), key=operator.itemgetter(1))[selected_index][1]}')
+
+
+def make_beautiful():
+    with open(csv_uri, 'r') as csv_file:
+        user_dict = {}
+        csv_reader = csv.reader(csv_file, delimiter=csv_delimiter)
+        for row in csv_reader:
+            if row[0] in user_dict:
+                user_dict.update({row[0]: user_dict[row[0]] + 1})
+            else:
+                user_dict[row[0]] = 1
+
+        remained_user_list = []
+        should_delete_number = 1900000
+        for key, value in sorted(user_dict.items(), key=operator.itemgetter(1)):
+            if should_delete_number < 0:
+                remained_user_list.append(key)
+            should_delete_number -= 1
+
+        print(f'Number of remained users: {len(remained_user_list)}')
+
 if __name__ == '__main__':
     warehouse = hooshex.warehouse
+
+    # calculate_rows()
+    # calculate_unique_users()
+    # calculate_unique_entities()
+    # calculate_user_activities()
+
+
 
     timer = HTimer()
 
     # timer.start()
     # read()
     # timer.end_and_print()
-    #
+
     # timer.start()
     # warehouse.save('../../datasource/ratings_Movies_and_TV.gt')
     # timer.end_and_print()
@@ -64,4 +172,16 @@ if __name__ == '__main__':
     warehouse.load('../../datasource/ratings_Movies_and_TV.gt')
     timer.end_and_print()
 
-    print('Hello')
+    timer.start()
+    find_usable()
+    timer.end_and_print()
+
+    cpu = hooshex.cpu
+    while True:
+        inp = input("Type 2 param (separate with `,`)\n").split(',')
+
+        result = hooshex.cpu.calculate_smart_score(inp[0], inp[1])
+        if result and len(result) > 0:
+            print('%s item %s' % (len(result), int(functools.reduce(operator.add, result, 1) / len(result))))
+
+            # print('Hello')
